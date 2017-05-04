1d88734cce556e1a017f5f9447e22214a334feb6
Supriya Somashekar <supriya@Supriyas-MacBook-Pro.local>
2017-05-03 17:36:05 -0700

    code with schedules

--- keywords_location_table.py
+++ keywords_location_table.py
@@ -5,7 +5,7 @@ import csv
 db = sqlite3.connect('chatbot.db')
 
 cur = db.cursor()
-rd=csv.reader(open('/Users/supriya/Applications/keywords.csv','r'),delimiter=',')
+rd=csv.reader(open('/Users/supriya/Applications/seats_keywords.csv','r'),delimiter=',')
 for row in rd:
         to_db=[unicode(row[0], "utf8"), unicode(row[1], "utf8"),unicode(row[2], "utf8"),unicode(row[3], "utf8")]
         st="INSERT INTO data (keywords, clmn, tbl, source) VALUES(?,?,?,?);"
--- location_timing.py
+++ location_timing.py
@@ -39,48 +39,46 @@ def handle_command(command, channel):
 
 def query_keywords(keywords,name, channel):
     db = sqlite3.connect('chatbot.db')
-    #print name
-    #print keywords
     cur = db.cursor()
     result = cur.execute('SELECT clmn, tbl, source FROM data WHERE keywords = :k', {"k":keywords})
     row = cur.fetchone()
-    #print row
     if row == None:
         response = "I am yet to learn that"
         slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
     else:
-        #row = cur.fetchone()
-        #print row
-        #print row[0], row[1], row[2]
         query = 'SELECT {} FROM {} WHERE {} = :n'.format(row[0], row[1], row[2])
-        #print query
         cur.execute(query, {"n":name})
         res = cur.fetchall()
-        #print res
 
         if res == []:
             response = "I am yet to learn that"
             slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
         elif res > 0:
-            #res = cur.fetchall()
-            #print res
-            #response = "Please spicify the request with the section number!"
-            #slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
-            for r in res:
-                str(r[0])
-                int(r[1])
-                a = str(r[1])
-                a.split(".")
-                #print r[0]
-                #print r[1]
-                response = "Section " + a[0:len(a)-2] + " is in " + r[0]
-                slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+            if row[0] == "location, section":
+                for r in res:
+                    str(r[0])
+                    int(r[1])
+                    a = str(r[1])
+                    a.split(".")
+                    #print r[0]
+                    #print r[1]
+                    response = "Section " + a[0:len(a)-2] + " is in " + r[0]
+                    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+            elif row[0] ==  "section, class_day, class_time":
+                for r in res:
+                    str(r[1])
+                    int(r[0])
+                    str(r[2])
+                    a = str(r[0])
+                    a.split(".")
+                    #print r[0]
+                    #print r[1]
+                    #print r[2]
+                    response = "Section " + a[0:len(a)-2] + " schedule is " + r[1] + " " + r[2]
+                    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
         else:
-            #res = cur.fetchall()
-            #print res
-            #for r in res:
-            response = res   
+            response = res 
             slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
     db.close()
 
@@ -89,7 +87,6 @@ def parse_slack_output(slack_rtm_output):
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip(), \
                        output['channel']
     return None, None
@@ -106,4 +103,4 @@ if __name__ == "__main__":
                 handle_command(command, channel)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
-        print("Connection failed. Invalid Slack token or bot ID?")
+        print "Connection failed. Invalid Slack token or bot ID?"
